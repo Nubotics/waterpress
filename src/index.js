bd27aa24145f11dba9b155116ab754fefb7a41ef
Barry Buck <jozi011@gmail.com>
2015-07-13 04:09:27 +0000

    setup export

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
  * Created by nubuck on 15/07/09.
  */
 
+import WaterpressOrm from './core/WaterpressOrm'
+import WaterpressApi from './core/WaterpressApi'
+
+export {
+  WaterpressOrm,
+  WaterpressApi
+}
