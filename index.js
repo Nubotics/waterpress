4b21aedf268adb5e926961a56efd5ff0167b9137
Barry Buck <jozi011@gmail.com>
2015-09-08 09:44:55 +0000

    rebuild

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-//module.exports = require('./src/index')
-module.exports = require('./lib/index')
+var env = require('get-env')({
+  development: ['development', 'dev', 'local'],
+  production: ['production', 'prod', 'live', 'staging']
+})
+
+module.exports.env = env;
+
+if (env === 'production'){
+  module.exports = require('./lib/index');
+}else{
+  module.exports = require('./src/index');
+}
