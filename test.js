ec612f9f7ed3007dd54eee9eae49aca01145a629
Barry Buck <jozi011@gmail.com>
2015-11-19 04:45:17 +0000

    v 0.2.1 wip

--- test.js
+++ test.js
@@ -8,6 +8,8 @@ import wp, {
 
 } from './src'
 const {_} = u
+import chalk from 'chalk'
+import stringifyObject from 'stringify-object'
 
 //plugins
 
@@ -28,20 +30,35 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     console.log('done end')
   }
   const inspectResult = function (err, result) {
-    let report = 'result: '
+    let report = 'result:\n'
 
     const inspectObject = (object)=> {
-
+      report += `-> ${chalk.green('object returned')}\n`
+      report += `--> ${chalk.yellow('keys:')}\n`
+      _.mapKeys(object,(value,key)=>{
+        report += `---> ${chalk.yellow(key)}\n`
+      })
+      //TODO: further inspect contents of object
     }
     const inspectArray = (array)=> {
+      report += `-> ${chalk.green('array returned')}\n`
+      report += `--> ${chalk.magenta('array length:')} ${array.length}\n`
+      //TODO: further inspect contents of array
 
     }
     const inspectValue = (value)=> {
+      report += `-> ${chalk.green('value returned')}\n`
+      report += `--> ${chalk.cyan('value:')} ${value}\n`
+    }
 
+    let stringOpts = {
+      indent: '  ',
+      singleQuotes: true
     }
 
     if (err) {
-
+      report += `${chalk.red('err')}\n`
+      report += `--> ${stringifyObject(err,stringOpts)}`
     } else if (_.isObject(result)) {
       if (_.isArray(result)) {
         inspectArray(result)
@@ -93,7 +110,7 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
 
     .plug((context, next)=> {
       //context = api -> this
-      console.log('plug -> context -> ', inspectResult(context))
+      console.log('plug -> context -> ', inspectResult(null, context))
       console.log('\n')
       next()
     })
@@ -105,27 +122,27 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('postId', 0)
     .post
     .find({}, {limit: 1, skip: 3}, (err, result, next)=> {
-      console.log('post -> find -> ', inspectResult(result))
+      console.log('post -> find -> ', inspectResult(err, result))
       next()
     })
     .older(100, (err, result, next)=> {
-      console.log('post -> older -> ', inspectResult(result))
+      console.log('post -> older -> ', inspectResult(err, result))
       next()
     })
     .newer(100, (err, result, next)=> {
-      console.log('post -> newer -> ', inspectResult(result))
+      console.log('post -> newer -> ', inspectResult(err, result))
       next()
     })
     .one({}, (err, result, next)=> {
-      console.log('post -> one -> ', inspectResult(result))
+      console.log('post -> one -> ', inspectResult(err, result))
       next()
     })
     .save({/* stub post here */}, (err, result, next)=> {
-      console.log('post -> save -> ', inspectResult(result))
+      console.log('post -> save -> ', inspectResult(err, result))
       next()
     })
     .findChildren(100, (err, result, next)=> {
-      console.log('post -> findChildren -> ', inspectResult(result))
+      console.log('post -> findChildren -> ', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -138,27 +155,27 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('userId', 0)
     .user
     .find({id: {'>': 10}}, (err, result, next)=> {
-      console.log('user -> find ->', inspectResult(result))
+      console.log('user -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('user -> one ->', inspectResult(result))
+      console.log('user -> one ->', inspectResult(err, result))
       next()
     })
     .byRole('rolename', (err, result, next)=> {
-      console.log('user -> byRole ->', inspectResult(result))
+      console.log('user -> byRole ->', inspectResult(err, result))
       next()
     })
     .existsByEmail('email', (err, result, next)=> {
-      console.log('user -> existsByEmail ->', inspectResult(result))
+      console.log('user -> existsByEmail ->', inspectResult(err, result))
       next()
     })
     .save({/* stub user here */}, (err, result, next)=> {
-      console.log('user -> save ->', inspectResult(result))
+      console.log('user -> save ->', inspectResult(err, result))
       next()
     })
     .checkLogin('email', 'password', (err, result, next)=> {
-      console.log('user -> checkLogin ->', inspectResult(result))
+      console.log('user -> checkLogin ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -171,11 +188,11 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('termId', 0)
     .term
     .find({name: {'contains': 'test'}}, (err, result, next)=> {
-      console.log('term -> find ->', inspectResult(result))
+      console.log('term -> find ->', inspectResult(err, result))
       next()
     })
     .byTaxonomy({name: {'contains': 'test'}}, (err, result, next)=> {
-      console.log('term -> byTaxonomy ->', inspectResult(result))
+      console.log('term -> byTaxonomy ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -188,19 +205,19 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('commentId', 0)
     .comment
     .find({term: {'>': 1}}, (err, result, next)=> {
-      console.log('comment -> find ->', inspectResult(result))
+      console.log('comment -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('comment -> one ->', inspectResult(result))
+      console.log('comment -> one ->', inspectResult(err, result))
       next()
     })
     .save({/* comment stub */}, (err, result, next)=> {
-      console.log('comment -> save ->', inspectResult(result))
+      console.log('comment -> save ->', inspectResult(err, result))
       next()
     })
     .kill(1, (err, result, next)=> {
-      console.log('comment -> kill ->', inspectResult(result))
+      console.log('comment -> kill ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -215,11 +232,11 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('categoryId', 0)
     .category
     .find({term: {'>': 1}}, false, (err, result, next)=> {
-      console.log('category -> find ->', inspectResult(result))
+      console.log('category -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('category -> one ->', inspectResult(result))
+      console.log('category -> one ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -232,7 +249,7 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('postFormat', 'post')
     .format
     .find({}, (err, result, next)=> {
-      console.log('format -> find ->', inspectResult(result))
+      console.log('format -> find ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -245,19 +262,19 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('mediaId', 0)
     .media
     .find({}, (err, result, next)=> {
-      console.log('media -> find ->', inspectResult(result))
+      console.log('media -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('media -> one ->', inspectResult(result))
+      console.log('media -> one ->', inspectResult(err, result))
       next()
     })
     .save({/* media stub */}, (err, result, next)=> {
-      console.log('media -> save ->', inspectResult(result))
+      console.log('media -> save ->', inspectResult(err, result))
       next()
     })
     .kill(1, (err, result, next)=> {
-      console.log('media -> kill ->', inspectResult(result))
+      console.log('media -> kill ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -270,19 +287,19 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('pageId', 0)
     .page
     .find({}, (err, result, next)=> {
-      console.log('page -> find ->', inspectResult(result))
+      console.log('page -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('page -> one ->', inspectResult(result))
+      console.log('page -> one ->', inspectResult(err, result))
       next()
     })
-    .save({/* media stub */}, (err, result, next)=> {
-      console.log('page -> save ->', inspectResult(result))
+    .save({/* page stub */}, (err, result, next)=> {
+      console.log('page -> save ->', inspectResult(err, result))
       next()
     })
     .kill(1, (err, result, next)=> {
-      console.log('page -> kill ->', inspectResult(result))
+      console.log('page -> kill ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
@@ -295,11 +312,11 @@ import pluginWpCategoryMeta from './src/plugins/wp-category-meta'
     .set('pageId', 0)
     .page
     .find({}, (err, result, next)=> {
-      console.log('tag -> find ->', inspectResult(result))
+      console.log('tag -> find ->', inspectResult(err, result))
       next()
     })
     .one({id: 1}, (err, result, next)=> {
-      console.log('tag -> one ->', inspectResult(result))
+      console.log('tag -> one ->', inspectResult(err, result))
       next()
     })
     .done(next=> {
