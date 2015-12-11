46769f8d9ada0b3300af97d9683d2f3bf7a6d161
Barry Buck <jozi011@gmail.com>
2015-12-11 19:14:42 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import {
 
 import models from './models'
 
+import addons from './addons'
+
 export {
   EventApi,
   Api,
   Orm,
   u,
   models,
-
+  addons,
 }
 
 export default function (options){
